990bb8e2927ed440cd537aff165adab6cc76ce0d
taise <taise515@gmail.com>
2013-07-12 21:33:10 +0900

    Add arrival route

--- app/controllers/Materia.java
+++ app/controllers/Materia.java
@@ -14,9 +14,22 @@ import play.db.ebean.Model.Finder;
 public class Materia extends Controller {
   
     public static Result index() {
-      Finder<Long, Product> finder = new Finder<Long, Product>(Long.class, Product.class);
-      List<Product> products = finder.all();
+      List<Product> products = productsAll();
         return ok(views.html.product.index.render(products));
     }
   
+    public static Result newArrival() {
+      return ok(views.html.product.arrival.render());
+    }
+
+    public static Result arrival() {
+      List<Product> products = productsAll();
+      return ok(views.html.product.index.render(products));
+    }
+
+    protected static List<Product> productsAll() {
+      Finder<Long, Product> finder = new Finder<Long, Product>(Long.class, Product.class);
+      List<Product> products = finder.all();
+      return products;
+    }
 }
