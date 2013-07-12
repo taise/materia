4f7baa027ef9300bc01e7523659e872d93e87e09
taise <taise515@gmail.com>
2013-07-12 19:46:56 +0900

    Create product Liting View

--- app/controllers/Materia.java
+++ app/controllers/Materia.java
@@ -14,22 +14,9 @@ import play.db.ebean.Model.Finder;
 public class Materia extends Controller {
   
     public static Result index() {
-      Product product = new Product();
-      product.name = "えんぴつ";
-      product.price = new Long(100);
-      product.quantity = new Long(250);
-      product.location = "東京";
-      product.updateDate = new Date();
-      product.save();
-
       Finder<Long, Product> finder = new Finder<Long, Product>(Long.class, Product.class);
       List<Product> products = finder.all();
-      StringBuilder msg = new StringBuilder();
-      msg.append("Products:\n");
-      for (Product p : products) {
-                  msg.append(p.toString()).append("\n");
-      }
-        return ok(msg.toString());
+        return ok(index.render(products));
     }
   
 }
